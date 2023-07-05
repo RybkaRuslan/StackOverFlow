4ab50e7fc528ea25cf037166959955b88ecd5b11
Ruslan <ruslanrybka10@gmail.com>
2023-07-06 00:22:33 +0300

    MVP version

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import SearchScreen from "./SearchScreen/SearchScreen";
+import QuestionInfoScreen from "./QuestionInfoScreen/QuestionInfoScreen";
+import SearchResultsScreen from "./SearchResultsScreen/SearchResultsScreen";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route index element={<SearchScreen />} />
+        <Route path="/results" element={<SearchResultsScreen />} />
+        <Route path="/question/:questionId" element={<QuestionInfoScreen />} />
+      </Routes>
     </div>
   );
 }
